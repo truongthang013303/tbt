3495b71f1f6a74be6440a01ea0c094adb8233864
bt <truongthang013303.work@gmail.com>
2023-06-23 21:00:52 +0700

    early start

---
+++ src/main/java/com/world/tbt/TbtApplication.java
@@ -0,0 +1,13 @@
+package com.world.tbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/world/tbt/controller/web/HomeController.java
@@ -0,0 +1,14 @@
+package com.world.tbt.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller(value = "homeControllerOfWeb")
+public class HomeController {
+    @RequestMapping(value = {"/home","/"}, method = RequestMethod.GET)
+    public String homeUser()
+    {
+        return "/web/Home/WorldHome";
+    }
+}
