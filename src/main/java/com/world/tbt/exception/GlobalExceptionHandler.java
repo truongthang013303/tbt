6163216459c72c0c77770ba7b029efec79aa9b39
bt <truongthang013303.work@gmail.com>
2023-06-23 23:21:36 +0700

    [fix]GlobalExceptionHandler

--- src/main/java/com/world/tbt/exception/GlobalExceptionHandler.java
+++ src/main/java/com/world/tbt/exception/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
